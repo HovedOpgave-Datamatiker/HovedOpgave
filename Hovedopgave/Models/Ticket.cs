fb853eff40b0717c258e453982472f57f8b8e230
philipbenn <phil658c@stud.kea.dk>
2024-12-02 14:21:40 +0100

    Realation between User and Ticket created

--- Hovedopgave/Models/Ticket.cs
+++ Hovedopgave/Models/Ticket.cs
@@ -1,20 +1,28 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Hovedopgave.Models
 {
     public class Ticket
     {
-
         public int Id { get; set; }
         public string Description { get; set; }
+
         [DisplayName("Ticket Status")]
         public bool IsFinished { get; set; }
         public DateTime Created { get; set; }
+
         [DisplayName("Last Update")]
         public DateTime LastUpdated { get; set; }
+
         [DisplayName("Ticket Priority")]
         public int Priority { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
 
         public Ticket()
         {
@@ -22,7 +30,5 @@ namespace Hovedopgave.Models
             Created = DateTime.Now;
             LastUpdated = DateTime.Now;
         }
-
-
     }
 }
--- Hovedopgave/Models/User.cs
+++ Hovedopgave/Models/User.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Hovedopgave.Models
 {
@@ -11,5 +12,12 @@ namespace Hovedopgave.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; }
+
+        public User()
+        {
+            Tickets = new List<Ticket>();
+        }
     }
 }
